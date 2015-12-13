1468d9c72d7df4c56cc46610a12be89e82ce0320
Dino Prašo <desmond1303@gmail.com>
2015-12-13 12:15:00 +0100

    Today Widget Structure

---
+++ Project Cinema: Upcoming/TodayViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TodayViewController.swift
+//  Project Cinema: Upcoming
+//
+//  Created by Dino Praso on 13.12.15.
+//  Copyright © 2015 Dino Praso. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+import RealmSwift
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let realm = try! Realm()
+        print(realm.objects(PCMediaItem).filter("release_date > '2015-12-13'"))
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+
+        completionHandler(NCUpdateResult.NewData)
+    }
+    
+}
